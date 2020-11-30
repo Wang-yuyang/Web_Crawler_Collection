b58f2d919ad3e91058937959a814d32fc40b7f04
Wang-yuyang <44972408+Wang-yuyang@users.noreply.github.com>
2020-11-30 13:29:55 +0800

    upload job_search.py

---
+++ Job_search.py
@@ -0,0 +1,19 @@
+#! python3
+
+import requests
+
+from bs4 import BeautifulSoup
+
+
+def lagouCrawler():
+    pass
+
+def bossCrawler():
+    pass
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
